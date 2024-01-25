8309783f8fbc670b7f48be24ee00705810489491
Harry Linrui XU <xulr0820@hotmail.com>
2024-01-25 13:58:06 +0100

    Committing new files

---
+++ øving3/calculator-v2/src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <Calculator class="calculator" @calculate="addToLog"/>
+  <Log class="log" :equation-log="log" />
+</template>
+
+<style scoped>
+.calculator, .log {
+  /*Flex and width doesn't seem to affect the final layout, but keeping it to show possibility*/
+  flex: 1; /*ensure both components take equal height */
+  width: 100%; /*both components take 100% width of the parent container */
+  border-radius: 3%;
+  max-width: 300px;
+}
+</style>
+
+<script setup lang="ts">
+import Calculator from '@/components/Calculator.vue';
+import Log from '@/components/Log.vue'
+import { ref } from 'vue'
+
+const log = ref([] as string[]);
+const addToLog = (expression: string) => {
+  log.value.unshift(expression);
+};
+</script>
+
+<!--
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+-------------------
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+---------------
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+-->
