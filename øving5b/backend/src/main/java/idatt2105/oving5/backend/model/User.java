0e7f326c4cf4569527a30f80098fb6015bcf0359
harui <xulr0820@hotmail.com>
2024-03-10 20:19:36 +0100

    Added lombok dependecy and removed boiler-plate code in User

--- øving5b/backend/src/main/java/idatt2105/oving5/backend/model/User.java
+++ øving5b/backend/src/main/java/idatt2105/oving5/backend/model/User.java
@@ -9,9 +9,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
@@ -20,13 +29,6 @@ public class User {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  public Long getId(){
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
   private String username;
 
   private String password;
@@ -35,44 +37,8 @@ public class User {
   @JoinColumn(name = "user_id")
   private List<Equation> equations = new ArrayList<>();
 
-  public User(String username, String password) {
-    this.username = username;
-    this.password = password;
-  }
-  public User() {
-
-  }
   public void addEquation(Equation equation) {
     equations.add(equation);
   }
 
-  public List<Equation> getEquations() {
-    return equations;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  @Override
-  public String toString() {
-    return "User{" +
-        "id=" + id +
-        ", username='" + username + '\'' +
-        ", password='" + password + '\'' +
-        ", equations=" + equations +
-        '}';
-  }
 }
