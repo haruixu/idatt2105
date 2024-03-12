a06e2992a6fc69c19ab354a85acf6ca7cc7b0495
harui <xulr0820@hotmail.com>
2024-03-13 00:20:52 +0100

    Session storage import

--- øving5b/frontend/src/main.ts
+++ øving5b/frontend/src/main.ts
@@ -1,12 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedState from "pinia-plugin-persistedstate"
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedState)
 
 app.use(createPinia())
 app.use(router)
