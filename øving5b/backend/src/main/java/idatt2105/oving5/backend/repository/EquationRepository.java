558ced3905d6a2b9c15ecd6f39e5daa48b447a6d
Harry Linrui XU <xulr0820@hotmail.com>
2024-03-20 15:07:25 +0100

    format

--- øving5b/backend/src/main/java/idatt2105/oving5/backend/repository/EquationRepository.java
+++ øving5b/backend/src/main/java/idatt2105/oving5/backend/repository/EquationRepository.java
@@ -4,5 +4,4 @@ import idatt2105.oving5.backend.model.Equation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EquationRepository extends JpaRepository<Equation, Long> {
-
 }
